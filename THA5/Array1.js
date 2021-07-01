e3d8660e1c08e7972b5b2efe3a6666873e6c0d23
ANUPAM-RO <anupamroy575@gmail.com>
2021-07-01 13:35:55 +0530

    inital commit

---
+++ THA5/Array1.js
@@ -0,0 +1,7 @@
+/*Write a JavaScript function to check whether an `input` is an array or not*/
+a1 = [1,2,4,0]
+a2 = ('Anupam');
+
+console.log(Array.isArray(a1));
+console.log(Array.isArray(a2));
+
