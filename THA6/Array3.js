7f98b806233319d54d64982305725853dffa0d89
ANUPAM-RO <anupamroy575@gmail.com>
2021-07-02 13:53:55 +0530

    add THA5 and modify THA6

--- THA6/Array3.js
+++ THA6/Array3.js
@@ -1,7 +1,13 @@
 /* Write a JavaScript function to get the first element of an array. Passing a
 parameter 'n' will return the first 'n' elements of the array */
-var first = function(n) {
-   return n.slice(0,n)
+var first = function(arr,n) {
+   if (arr == null) 
+   return void 0;
+ if (n == null) 
+   return arr[0];
+ if (n < 0)
+   return [];
+ return arr.slice(0, n);
 }
 console.log(first([7, 9, 0, -2]));
 console.log(first([],3));
